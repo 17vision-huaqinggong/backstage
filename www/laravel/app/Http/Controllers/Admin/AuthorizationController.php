bf2658723ed9bea2fc151655fd057278d6831155
zhoulin <zhoulin@17vision.com>
2024-02-28 15:47:13 +0800

    增加登录和获取用户接口

---
+++ www/laravel/app/Http/Controllers/Admin/AuthorizationController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use App\Models\User;
+
+class AuthorizationController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'account' => ['required'],
+            'password' => 'required|string|max:16',
+        ], [], [
+            'account' => '账号',
+            'password' => '密码',
+        ]);
+
+        $data = $request->only(['account','password']);
+
+        if (Auth::validate($data)) {
+            $user = User::where('account', $data['account'])->first();
+
+            $token = $user->createToken('auth')->plainTextToken;
+
+            return response()->json(['token' => $token]);
+        } else {
+            return response()->json(['message' => '账号或密码错误'], 403);
+        }
+    }
+
+    public function getUserInfo(Request $request)
+    {
+        $user = $request->user();
+
+        $user = $user->only(['id', 'nickname', 'phone', 'gender', 'avatar', 'email']);
+
+        return response()->json($user);
+    }
+
+    public function setPassword(Request $request)
+    {
+        $request->validate([
+            'password' => 'required|min:6|max:16',
+            'captcha_code' => 'required|string',
+            'captcha_key' => 'required|string',
+        ], [
+
+        ], [
+            'password' => '密码',
+            'captcha_code' => '验证码',
+            'captcha_key' => '验证码',
+        ]);
+
+        $password = $request->password;
+
+        $captchaData = Cache::get('captcha_' . $request->captcha_key);
+        if (!$captchaData) {
+            return response()->json(['message' => '验证码已失效'], 403);
+        }
+
+        if (!hash_equals($captchaData['code'], strtolower($request->captcha_code))) {
+            // 验证错误就清除缓存
+            // Cache::forget('captcha_' . $request->captcha_key);
+            return response()->json(['message' => '验证码错误'], 403);
+        }
+        Cache::forget('captcha_' . $request->captcha_key);
+
+        $password = Hash::make($password);
+
+        $result = ($request->user())->update(['password' => $password]);
+
+        return response()->json($result);
+    }
+}
--- www/laravel/routes/admin.php
+++ www/laravel/routes/admin.php
@@ -1,17 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthorizationController;
 
 Route::get('hello', function(){
     return 'admin api say: hello';
 });
 
 Route::middleware(['throttle:' . config('api.rate_limits.sign')])->group(function () {
-
+    // 登录
+    Route::post('login', [AuthorizationController::class, 'login']);
 });
 
 Route::group([
     'middleware' => ['auth:admin'],
 ], function () {
-
+    Route::get('user/info', [AuthorizationController::class, 'getUserInfo']);
 });
